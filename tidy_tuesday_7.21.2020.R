a88d116d3e44b1918c509c90934a55c3bccb7179
Daniel Molitor <dmolitor@ripl.org>
2020-07-21 21:04:10 -0400

    Script for 2020-07-21

---
+++ tidy_tuesday_7.21.2020.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+library(ggplot2)
+library(viridis)
+library(cowplot)
+
+animal_outcomes <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-21/animal_outcomes.csv')
+animal_complaints <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-21/animal_complaints.csv') %>%
+  mutate(Suburb = str_replace_all(Suburb, "Mount", "Mt"))
+brisbane_complaints <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-21/brisbane_complaints.csv',
+                                col_types = cols(.default = "c"))
+# Pivot animal_outcomes longer
+animal_outcomes_long <- animal_outcomes %>%
+  pivot_longer(cols = ACT:WA,
+               names_to = "region",
+               values_to = "count") %>%
+  mutate(region = case_when(region == "ACT" ~ "Australian Capital Territory",
+                            region == "NSW" ~ "New South Wales",
+                            region == "NT"  ~ "Northern Territory",
+                            region == "QLD" ~ "Queensland",
+                            region == "SA"  ~ "South Australia",
+                            region == "TAS" ~ "Tasmania",
+                            region == "VIC" ~ "Victoria",
+                            region == "WA"  ~ "Western Australia")) %>%
+  group_by(outcome) %>%
+  mutate(outcome_n = sum(count, na.rm = TRUE)) %>%
+  ungroup() %>%
+  group_by(region) %>%
+  mutate(region_n = sum(count, na.rm = TRUE)) %>%
+  ungroup()
+
+ggplot(animal_outcomes_long %>%
+         mutate(outcome = factor(outcome,
+                                 levels = animal_outcomes_long %>%
+                                   distinct(outcome, .keep_all = TRUE) %>%
+                                   arrange(desc(outcome_n)) %>%
+                                   pull(outcome),
+                                 ordered = TRUE))) +
+  geom_bar(aes(x = factor(year),
+               y = count,
+               fill = outcome),
+           stat = "identity") +
+  theme_minimal_hgrid() +
+  theme(legend.title = element_blank(),
+        axis.text.x = element_text(angle = 90)) +
+  scale_fill_viridis(option = "plasma",
+                     discrete = TRUE,
+                     direction = 1) +
+  labs(x = "", y = "")
+
+ggplot(animal_outcomes_long %>%
+         mutate(outcome = factor(outcome,
+                                 levels = animal_outcomes_long %>%
+                                   distinct(outcome, .keep_all = TRUE) %>%
+                                   arrange(desc(outcome_n)) %>%
+                                   pull(outcome),
+                                 ordered = TRUE),
+                region = factor(region,
+                                levels = animal_outcomes_long %>%
+                                  distinct(region, .keep_all = TRUE) %>%
+                                  arrange(desc(region_n)) %>%
+                                  pull(region),
+                                ordered = TRUE))) +
+  geom_bar(aes(x = factor(year),
+               y = count,
+               fill = outcome),
+           stat = "identity") +
+  facet_wrap(~ region,
+             ncol = 2) +
+  theme_minimal_hgrid() +
+  theme(legend.title = element_blank(),
+        axis.text.x = element_text(angle = 90)) +
+  scale_fill_viridis(option = "plasma",
+                     discrete = TRUE,
+                     direction = 1) +
+  labs(x = "", y = "")
